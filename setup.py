5a691b1820af579274bf9bbd4274543443d82467
prakhathi-m <36877395+prakhathi-m@users.noreply.github.com>
2019-11-20 23:10:47 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Tweet-Search',
+    version='1.0.0',
+    description='A Template for creating a Full Stack Web Application using Python, NPM, Webpack and React',
+    keywords = [ 'fullstack', 'template', 'python', 'react','npm', 'webpack'],
+    classifiers = [
+    'Topic :: Software Development',
+    'Natural Language :: English'
+    ],
+    requires=['flask']
+)
